e416501ff96715a9b8c2c71d6f45c7c0a804761f
rkoh-rq <49420412+rkoh-rq@users.noreply.github.com>
2023-03-18 20:53:25 +0800

    add OoD detection head

---
+++ models/head.py
@@ -0,0 +1,60 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+
+class ResizeHead(nn.Module):
+    def __init__(self, out):
+        super().__init__()
+        self.pool = nn.AdaptiveAvgPool2d((10,10))
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 9, 5)
+        self.conv3 = nn.Conv2d(9, 12, 5)
+        self.conv4 = nn.Conv2d(12, 9, 5)
+        self.conv5 = nn.Conv2d(9, 6, 5)
+        self.fc1 = nn.Linear(6 * 10 * 10, 120)
+        self.fc2 = nn.Linear(120, 32)
+        self.fc3 = nn.Linear(32, out)
+        self.dropout = nn.Dropout(p=0.1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = self.pool(F.relu(self.conv5(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        # x = self.dropout(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+
+class ResizeHeadClass(nn.Module):
+    def __init__(self, out):
+        super().__init__()
+        self.pool = nn.AdaptiveAvgPool2d((5,5))
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.conv2 = nn.Conv2d(6, 12, 5)
+        self.conv3 = nn.Conv2d(12, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 32)
+        self.fc3 = nn.Linear(32, out)
+        self.dropout = nn.Dropout(p=0.1)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = self.dropout(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return self.softmax(x)
---
+++ test_head.py
@@ -0,0 +1,70 @@
+import argparse
+import json
+import os
+from pathlib import Path
+from threading import Thread
+
+import numpy as np
+import torch
+import torchvision.transforms as T
+import yaml
+from tqdm import tqdm
+
+from models.experimental import attempt_load
+from models.head import ResizeHead
+from utils.datasets import create_dataloader
+from utils.general import coco80_to_coco91_class, check_dataset, check_file, check_img_size, check_requirements, \
+    box_iou, non_max_suppression, scale_coords, xyxy2xywh, xywh2xyxy, set_logging, increment_path, colorstr
+from utils.loss import ComputeLoss
+from utils.metrics import ap_per_class, ConfusionMatrix
+from utils.plots import plot_images, output_to_target, plot_study_txt
+from utils.torch_utils import select_device, time_synchronized, TracedModel
+
+import pickle
+
+def test(data,
+         batch_size=32,
+         imgsz=640,
+         dataloader=None):
+    set_logging()
+    device = select_device(opt.device, batch_size=batch_size)
+    with open('filename.pickle', 'rb') as handle:
+            losses = pickle.load(handle)
+    
+    task = "train"
+    # Configure
+    if isinstance(data, str):
+        with open(data) as f:
+            data = yaml.load(f, Loader=yaml.SafeLoader)
+    check_dataset(data)  # check
+    dataloader = create_dataloader(data[task], imgsz, batch_size, 32, opt, pad=0.5, rect=True,
+                                    prefix=colorstr(f'{task}: '))[0]
+    head = ResizeHead((imgsz-192)//32).to(device)
+    checkpoint = torch.load("runs/train/head")
+    head.load_state_dict(checkpoint)
+    head.eval()
+
+    for img, _, paths, _ in tqdm(dataloader):
+        img = img.to(device, non_blocking=True)
+        img = img.float()
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        targets = torch.Tensor([losses[p] for p in paths]).to(device)
+        output = head(img)
+        print(targets, output)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='test_head.py')
+    parser.add_argument('--data', type=str, default='data/coco.yaml', help='*.data path')
+    parser.add_argument('--batch-size', type=int, default=32, help='size of each image batch')
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--single-cls', action='store_true', help='treat as single-class dataset')
+    parser.add_argument('--save-json', action='store_true', help='save a cocoapi-compatible JSON results file')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    opt = parser.parse_args()
+    opt.save_json |= opt.data.endswith('coco.yaml')
+    opt.data = check_file(opt.data)  # check file
+
+    test(opt.data,
+          opt.batch_size,
+          opt.img_size,
+          )
